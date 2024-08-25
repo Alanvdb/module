3e1987d11f36f2d69a1d140339837e0e531efb86
alanvdb <alanvdb.coder@gmail.com>
2024-08-25 23:01:38 +0200

    ImageEntity has altText

--- src/ImageEntity.php
+++ src/ImageEntity.php
@@ -7,4 +7,16 @@ use AlanVdb\Module\Common\HasPath;
 class ImageEntity extends PostEntity
 {
     use HasPath;
+
+    protected string $altText = '';
+
+    public function getAltText() : string 
+    {
+        return $this->altText;
+    }
+
+    public function setAltText(string $altText) : self
+    {
+        $this->altText = $altText;
+    }
 }
