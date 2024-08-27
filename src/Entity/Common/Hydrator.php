8d5050174d46c33025bf0bbf2beb7391bb0e1f4c
alanvdb <alanvdb.coder@gmail.com>
2024-08-27 21:37:11 +0200

    Hydrate return self

--- src/Entity/Common/Hydrator.php
+++ src/Entity/Common/Hydrator.php
@@ -4,10 +4,11 @@ namespace AlanVdb\Module\Entity\Common;
 
 trait Hydrator
 {
-    public function hydrate(array $attributes)
+    public function hydrate(array $attributes) : self
     {
         foreach ($attributes as $attrName => $attrValue) {
             $this->$attrName = $attrValue;
         }
+        return $this;
     }
 }
